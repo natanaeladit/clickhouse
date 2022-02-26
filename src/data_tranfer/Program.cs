c23db8d4d31df1565f46d33e7f903ec8765a8bf0
natanaeladit <nata.adit@gmail.com>
2022-02-26 21:30:30 +0800

    add data transfer project

---
+++ src/data_tranfer/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClickHouse.Client.ADO;
+using ClickHouse.Client.Utility;
+using Dapper;
+using System.Data.SqlClient;
+
+try
+{
+    using var sqlConnection = new SqlConnection(Environment.GetEnvironmentVariable("MSSQL_LOCAL"));
+    sqlConnection.Open();
+    var sqlVersion = await sqlConnection.ExecuteScalarAsync<string>("SELECT @@VERSION");
+    Console.WriteLine(sqlVersion);
+
+    using var clConnection = new ClickHouseConnection(Environment.GetEnvironmentVariable("CLICKHOUSE_LOCAL"));
+    var clVersion = await clConnection.ExecuteScalarAsync("SELECT version()");
+    Console.WriteLine(clVersion);
+    Console.Read();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.Read();
+}
